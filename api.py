c834d1baf5c33e1b822e50788fc5563a26cfc6f3
justinrsmith <justinrsmith88@gmail.com>
2018-03-10 14:55:27 -0600

    integrate dynamodb, create base note model

--- api.py
+++ api.py
@@ -1,7 +1,24 @@
+import uuid
+
 from flask import Flask, request, jsonify
+
+import boto3
+import os
 import requests
 
 app = Flask(__name__)
+
+NOTES_TABLE = os.environ['NOTES_TABLE']
+IS_OFFLINE = os.environ.get('IS_OFFLINE')
+
+if IS_OFFLINE:
+    client = boto3.client(
+        'dynamodb',
+        region_name='localhost',
+        endpoint_url='http://localhost:8000'
+    )
+else:
+    client = boto3.client('dynamodb')
 
 @app.route("/streamstatus", methods=["GET"])
 def stream_status():
@@ -13,3 +30,24 @@ def stream_status():
         response = jsonify({'online': False})
         response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+@app.route("/note", methods=["POST"])
+def create_note():
+    note_id = str(uuid.uuid4())
+    content = request.json.get('content')
+    attachment = request.json.get('attachement')
+    if not content:
+        return jsonify({'error': 'Must have content in your message.'})
+
+    resp = client.put_item(
+        TableName=NOTES_TABLE,
+        Item={
+            'noteId': {'S': note_id},
+            'content': {'S': content},
+            'attachment': {'S': attachment}
+        }
+    )
+    return jsonify({
+        'content': content,
+        'attachment': attachment
+    })
